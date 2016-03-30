53464710ac81dca9663f343cc9277d48efb0f5ea
Alan Alcesto <AlanAlcesto@Alans-MacBook-Pro.local>
2016-03-29 19:15:23 -0500

    add repo to desktop and added loop file to repo.

---
+++ java-practice/Loopy.java
@@ -0,0 +1,12 @@
+public class Loopy {
+	public static void main (String[] args) {
+		int x = 1;
+		System.out.print("Before the Loop");
+		while (x > 4) {
+			System.out.print("In the Loop");
+			System.out.print("Value of x is " + x);
+			x = x + 1;
+		}
+		System.out.print("This is after the loop");
+	}
+}
